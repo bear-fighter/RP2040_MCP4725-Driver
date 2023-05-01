385fa7facf613bd800bcf4e84f1e19f4e6b29f55
bear-fighter <105249199+bear-fighter@users.noreply.github.com>
2023-04-30 22:48:59 -0400

    permissions

--- mcp4725/rp2040_mcp4725.h
+++ mcp4725/rp2040_mcp4725.h
@@ -13,9 +13,15 @@
 #endif
 
 #include <stdint.h>
+#include <stdbool.h>
 
 
 #define MCP4725_ADDR 1100000
+#define NORMMODE 0b00
+#define PDMODE 0b10
+#define WR_FASTMODECMD 0b000
+#define WR_DACCMD 0b010
+#define WR_EEPROMCMD 0b011
 #define VCC 5f
 
 typedef struct mcp4725_data {
@@ -32,16 +38,16 @@ typedef struct mcp4725 {
 // Initialize instance of mcp4725 object
 mcp4725 mcp4725_Init(int address, int voltageset);
 
-// Set Voltage and Transmit
-// Reads the voltage back from the device and returns it as a value
-float mcp4725_VSet(mcp4725* dev, float volt);
-
 // Read from EEPROM and DAC Regsiter (Section 6.2)
 // returns 1 if read is sucessful; returns 0 otherwise
 uint8_t mcp4725_Read(mcp4725* dev);
 
 // (NEEDS PROTOTYPE) Fast Mode Write (Section 6.1.1)
-void mcp4725_FastWrite(mcp4725* dev);
+void mcp4725_FastWrite(mcp4725* dev, uint8_t pdsel, uint16_t dacval);
+
+// Set Voltage and Transmit
+// Reads the voltage back from the device and returns it as a value
+float mcp4725_VSet(mcp4725* dev, float volt);
 
 // (NEEDS PROTOTYPE) Write DAC Register (Section 6.1.2)
 
